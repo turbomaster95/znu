7836f9dc845516ace3f4666a456bbdcbf759d571
Deva Midhun <admin@coder.is-a.dev>
2026-04-29 19:17:10 +0530

    fixed init/main.c to have better ls.

--- init/main.c
+++ init/main.c
@@ -24,19 +24,44 @@ int main() {
             sys_shutdown();
         } else if (strcmp(line, "clear") == 0) {
             printf("\033[2J\033[H");
-        } else if (strcmp(line, "ls") == 0) {
-            int fd = sys_open("/", 0);
+	} else if (strncmp(line, "ls", 2) == 0) {
+            char path[256];
+            const char* input_path = line + 2;
+
+            // Skip leading spaces
+            while (*input_path == ' ') input_path++;
+
+            // If no path provided, default to root
+            if (*input_path == '\0') {
+                strcpy(path, "/");
+            } 
+            // If it doesn't start with '/', prepend one
+            else if (*input_path != '/') {
+                path[0] = '/';
+                strcpy(path + 1, input_path);
+            } 
+            // Otherwise, just copy it
+            else {
+                strcpy(path, input_path);
+            }
+
+            int fd = sys_open(path, 0);
             if (fd >= 0) {
                 znu_dirent_t dents[16];
-                int n = sys_getdents(fd, dents, sizeof(dents));
-                for (int i = 0; i < n / sizeof(znu_dirent_t); i++) {
-                    printf("%s  ", dents[i].name);
-                    if (dents[i].type == 2) printf("(dir)  ");
-                    printf("[%d bytes]\n", dents[i].size);
+                int bytes_read = sys_getdents(fd, dents, sizeof(dents));
+                
+                if (bytes_read > 0) {
+                    int count = bytes_read / sizeof(znu_dirent_t);
+                    for (int i = 0; i < count; i++) {
+                        printf("%s%s [%d bytes]\n", 
+                            dents[i].name, 
+                            (dents[i].type == 2 ? " (dir)" : ""), 
+                            dents[i].size);
+                    }
                 }
                 sys_close(fd);
             } else {
-                printf("Error opening /\n");
+                printf("ls: could not open '%s'\n", path);
             }
         } else if (strncmp(line, "cat ", 4) == 0) {
             const char* path = line + 4;
